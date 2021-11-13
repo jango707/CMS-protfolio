f9a907e006c909b3f8a60dfcfec66aae186e708a
Jang <jangbel99@gmail.com>
2021-11-13 13:59:33 +0000

    hook up posts

--- src/pages/notfound.js
+++ src/pages/notfound.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/layout"
-import "./pages.css"
 
 const NotFound = () => {
     return (
--- src/pages/post.js
+++ src/pages/post.js
@@ -3,28 +3,40 @@ import { Redirect } from "react-router-dom"
 import Markdown from "react-markdown"
 import Layout from "../components/layout"
 import postlist from "../posts.json"
-import "./pages.css"
+import "./post.css"
 
 const Post = (props) => {
-    const validId = props.match.params.slug
-    console.log(validId)
+    const validSlug = props.match.params.slug
     const fetchedPost = {}
     let postExists = false
     postlist.forEach((post, i) => {
-        if (validId === post.id) {
+        if (validSlug === convertToSlug(post.title)) {
             fetchedPost.title = post.title ? post.title : "No title given"
             fetchedPost.date = post.date ? post.date : "No date given"
-            fetchedPost.author = post.author ? post.author : "No author given"
             fetchedPost.content = post.content ? post.content : "No content given"
+            fetchedPost.image = post.thumbnail ? post.thumbnail : ""
             postExists = true
         }
     })
+
+    function convertToSlug(Text)
+    {
+        return Text
+            .toLowerCase()
+            .replace(/ /g,'-')
+            .replace(/[^\w-]+/g,'')
+            ;
+    }
     return (
         <Layout>
             <div className="post">
-                <h2>{fetchedPost.title}</h2>
-                <small>Published on {fetchedPost.date} by {fetchedPost.author}</small>
+                <h1>{fetchedPost.title}</h1>
+                <small>Published on {fetchedPost.date}</small><br />
                 <hr/>
+                <div className="img u-center">
+                    <img src={fetchedPost.image} alt="thumbnail" />
+                </div>
+                <a href="/projects" class="u u-LR" style={{padding:'0'}}>Return to Projects</a>
                 <Markdown source={fetchedPost.content} escapeHtml={false} />
             </div>
         </Layout>
