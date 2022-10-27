1cb20c613260741789d361df5b986af4fc9466b8
Jang <jangbel99@gmail.com>
2022-10-27 12:40:48 +0100

    Refactor projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,7 +17,9 @@ const Projects = () => {
 
           <div className="row">
             {projects
-              .sort(() => Math.random() - 0.5)
+              .sort(
+                (a, b) => projects.indexOf(a).date - projects.indexOf(b).date
+              )
               .map((project, i) => {
                 return (
                   <div
@@ -25,21 +27,6 @@ const Projects = () => {
                     className=" desktop col-lg-3 u-text-left col-md-4 col-sm-6 col-xs-12"
                   >
                     <ProjectCard onClick={onCardClick} project={project} />
-                  </div>
-                );
-              })}
-            {projects
-              .sort(() => Math.random() - 0.5)
-              .map((project, i) => {
-                return (
-                  <div
-                    key={i}
-                    className=" mobile col-lg-3 u-text-left col-md-4 col-sm-6 col-xs-12"
-                  >
-                    <SimpleProjectCard
-                      onClick={onCardClick}
-                      project={project}
-                    />
                   </div>
                 );
               })}
@@ -117,71 +104,6 @@ const ProjectCard = (props) => {
         </div>
         <div name={slug} className="card__footer content">
           {minutesToRead} min. {secondsToRead} sec. read | Click to read more.
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const SimpleProjectCard = (props) => {
-  const title = props.project.title;
-  const date = props.project.date;
-  const image = props.project.thumbnail;
-  const md_text = props.project.content;
-  const demo = props.project.demo;
-  const description = props.project.description;
-  const slug = convertToSlug(title);
-
-  const totalNumWords = wordsCount(md_text);
-  var secondsToRead = Math.round((totalNumWords / 250) * 60 + 1);
-  var minutesToRead = 0;
-
-  while (secondsToRead >= 60) {
-    minutesToRead++;
-    secondsToRead ^= 60;
-  }
-
-  function convertToSlug(Text) {
-    return Text.toLowerCase()
-      .replace(/ /g, "-")
-      .replace(/[^\w-]+/g, "");
-  }
-
-  return (
-    <div id="simplecard" name={slug} onClick={props.onClick}>
-      <div className="card" style={{ width: "100%" }}>
-        <div className="card__container">
-          <div
-            name={slug}
-            className="card__image"
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        </div>
-        <div className="content">
-          <h4>{title}</h4>
-          <div style={{ marginTop: "-15px", color: "grey" }}>
-            Published: {date}
-          </div>
-        </div>
-        <div
-          name={slug}
-          className="card__body content"
-          style={{ marginTop: "-10px" }}
-        >
-          <p name={slug}>{description}</p>
-        </div>
-        <div
-          className="card__footer content"
-          id="link-wrap"
-          style={{ marginBottom: "0px", fontSize: "15px" }}
-        >
-          Demo:{" "}
-          <a href={demo} className="" id="link">
-            {demo}
-          </a>
-        </div>
-        <div name={slug} className="card__footer content">
-          {minutesToRead} min. {secondsToRead} sec. read
         </div>
       </div>
     </div>
